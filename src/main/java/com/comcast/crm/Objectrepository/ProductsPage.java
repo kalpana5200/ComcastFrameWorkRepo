d57e78af7a85f12b64fcd59b4b668bd45ad020b9
kalpanapottepaka <sree386448@gmail.com>
2024-06-10 10:14:12 +0530

    Product, Added in POM class

---
+++ src/main/java/com/comcast/crm/Objectrepository/ProductsPage.java
@@ -0,0 +1,20 @@
+package com.comcast.crm.Objectrepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductsPage {
+	@FindBy (xpath="//img[@title='Create Product...']")
+	private WebElement createProdBtn;
+
+	public WebElement getCreateProdBtn() {
+		return createProdBtn;
+	}
+	
+	public ProductsPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+}
